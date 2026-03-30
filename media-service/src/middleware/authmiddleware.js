0f5885db0ec6af3fd09b1c1527fa92db5b301b59
manohar12-max <2898manohar@gmail.com>
2026-03-30 19:38:17 +0530

    task

--- media-service/src/middleware/authmiddleware.js
+++ media-service/src/middleware/authmiddleware.js
@@ -2,6 +2,10 @@
 const authenticateRequest=(req,res,next)=>{
     const userId=req.headers['x-user-id']
   if(!userId){
+    logger.warn("Access without valid userId")
+    logger.warn("Access without valid userId: %s", userId)
+    logger.warn("Access without valid userId: %s", userId)
+    logger.warn("Access without valid userId: %s", userId)
     return res.status(401).json({message:'Unauthorized'});
   }
     req.user={id:userId};
